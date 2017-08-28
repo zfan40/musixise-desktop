0a6e779f79440ad3b82237fc5dc34261e614e439
ziwen fan <zfan40@gatech.edu>
2017-08-28 23:08:15 +0800

    bind side bar navigation with router

--- app/src/renderer/components/SideBarView/index.vue
+++ app/src/renderer/components/SideBarView/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sidebar-wrapper">
-    <el-menu mode="vertical" default-active="1" class="el-menu-vertical-demo">
+    <el-menu mode="vertical" :default-active="activeIndex" class="el-menu-vertical-demo">
       <avatar-menu></avatar-menu>
       <menu-one></menu-one>
       <menu-two></menu-two>
@@ -14,6 +14,21 @@
   import MenuTwo from './MenuTwo';
 
   export default {
+    data() {
+      return {
+        activeIndex:'1'
+      }
+    },
+    watch: {
+      '$route' (to, from) {
+        // react to route changes...
+        if (to.path == '/recommend') {this.activeIndex = 1;console.log(1)}
+        else if (to.path == '/live') {this.activeIndex = 2;console.log(2)}
+        else if (to.path == '/shop') {this.activeIndex = 3;console.log(3)}
+        else if (to.path == '/favorite-musixisers') {this.activeIndex = 4}
+        else if (to.path == '/favorite-works') {this.activeIndex = 5}
+      }
+    },
     components: {
       'avatar-menu': AvatarMenu,
       'menu-one': MenuOne,
@@ -21,6 +36,8 @@
     },
     created() {
       console.log('--- SideBarView Init ---');
+      // active based on router /path
+      // this.activeIndex = this.$router.history.fullPath
     },
   };
 </script>
